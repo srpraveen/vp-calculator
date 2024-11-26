d1d3f2c042f8399eaf9ec0a26428240e5ceaa193
srpraveen <skr.srp@gmail.com>
2024-11-26 17:06:57 +0530

    Initial commit

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'vp-calculator-v1';
+const ASSETS_TO_CACHE = [
+    'index.html',
+    'manifest.json',
+    'images/icon-192x192.png',
+    'images/icon-512x512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Events
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return caches.match('index.html');
+            })
+    );
+});
